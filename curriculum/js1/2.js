5c3b37113d767af34039912a297f67c353847db0
michael <michaelbayday@garagescript.org>
2018-01-20 03:57:19 -0800

    #2

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -1,5 +1,11 @@
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, c) => {
+  if (a > b && a > c) {
+    return a;
+  }
+  if (b > a && b > c) {
+    return b;
+  }
+  return c;
 };
 
 module.exports = {
