e9396efa544eda54f2fa6e3f1d2f6b6b9475d7ad
michael <michaelbayday@garagescript.org>
2018-01-20 04:24:16 -0800

    4

--- curriculum/js1/4.js
+++ curriculum/js1/4.js
@@ -1,5 +1,8 @@
-const solution = (a, b) => {
-  return 0;
+const solution = (n, str, count = 0, finalStr = '') => {
+  if (count === n) {
+    return finalStr;
+  };
+  return solution(n, str, count + 1, finalStr + str);
 };
 
 module.exports = {
