39d8307f054e99f768cc3e5b1880e5b3137b6e60
michael <michaelbayday@garagescript.org>
2018-01-20 05:29:27 -0800

    3 new

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -2,7 +2,7 @@ const solution = (x, count = 0, str = '') => {
   if (x === count) {
    return str;
   };
-  return solution(x, count + 1, str + 'hello');
+  return solution(x, count + 1, `${str}hello`);
 };
 
 module.exports = {
